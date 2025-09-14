5dce3ce9934ddd334dd1096f05ea97e566c971a9
Jaysmart <ogunbunmijoshua60@gmail.com>
2025-09-14 19:09:16 +0100

    Fix token retrieval and enhance error handling in Dashboard component

--- Frontend/src/Pages/Dashboard/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react'
+import { useNavigate, Router } from 'react-router-dom';
 
 const Dashboard = () => {
 
-  const token = localStorage.getItem("auth.token")
+   const navigate = useNavigate();
+
+  const token = localStorage.getItem("auth_token")
 
   useEffect (() =>{
     axios.get("http://localhost:8005/verify", {
@@ -13,6 +16,13 @@ const Dashboard = () => {
       }
     }) .then((res)=>{
       console.log(res)
+    }) .catch ((err) => {
+      console.log(err);
+      const errormessage = err.response.data.message
+      if(errormessage == "jwt expired") {
+        localStorage.removeItem("auth_token")
+        navigate("/signin")
+      }
     })
     })
 
